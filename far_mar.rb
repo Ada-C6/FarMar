67cf38d0bbc49c5dc6cda23eb904a77ce2ab87f8
yaelittle <yaelittle@gmail.com>
2016-09-06 11:28:16 -0700

    base line

---
+++ far_mar.rb
@@ -0,0 +1,15 @@
+# FarMar/far_mar.rb
+
+# gems your project needs
+require 'csv'
+
+# ...require all needed classes
+require 'lib/markets_class.rb'
+require 'lib/products_class.rb'
+require 'lib/sales_class.rb'
+require 'lib/vendors_class.rb'
+
+# our namespace module
+module FarMar
+# all of our data classes that live in the module
+end
---
+++ lib/markets_class.rb
@@ -0,0 +1,7 @@
+# lib/markets._class.rb
+
+# Your implementation will have class methods to handle finding, sorting, and collecting the data into instances representing individual rows of data. Each of those instances will have instance methods to provide details about the object.
+
+class FarMar::Markets
+
+end
