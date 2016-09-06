2a744777facb236e6520b47c48649027633bac20
Jennifer Owens <jmo.jennifer@gmail.com>
2016-09-06 14:54:56 -0700

    Market self.all class written with TDD spec passed

--- lib/farmar_market/farmar_market.rb
+++ lib/farmar_market/farmar_market.rb
@@ -4,11 +4,29 @@ require 'csv'
 module FarMar
   class Market
 
-    attr_reader :market_csv
+    attr_reader :market_csv, :market_id, :market_name, :market_street, :market_city, :market_county, :market_state, :market_zip
 
-    def initialize
+    def initialize(market_hash)
       @market_csv = CSV.read("../FarMar/support/markets.csv", "r")
     end
 
+    def self.all
+      market_csv_file = CSV.read("../FarMar/support/markets.csv", "r")
+      market_array = []
+      market_hash = {}
+
+      market_csv_file.each do |market|
+        market_hash[:id] = market[0].to_i
+        market_hash[:name] = market[1]
+        market_hash[:street] = market[2]
+        market_hash[:city] = market[3]
+        market_hash[:market_county] = market[4]
+        market_hash[:state] = market[5]
+        market_hash[:zip] = market[6]
+        market_array.push(self.new(market_hash))
+      end
+
+      return market_array
+    end
   end
 end
--- specs/farmar_market_spec.rb
+++ specs/farmar_market_spec.rb
@@ -2,7 +2,7 @@ require_relative 'spec_helper'
 
 describe FarMar::Market do
 
-  let(:market) { FarMar::Market.new }
+  let(:market) { FarMar::Market.new({market_id: 6, market_name: "Test", market_street: "12345 Test Lane", market_city: "Seattle", market_county: "King", market_state: "WA", market_zip: "98107"}) }
 
   describe "#initialize" do
 
@@ -14,4 +14,13 @@ describe FarMar::Market do
       market.market_csv.wont_be_empty
     end
   end
+
+  describe "all" do
+    it "should put all values of its CSV file into an array" do
+      FarMar::Market.new({market_id: 6, market_name: "Test", market_street: "12345 Test Lane", market_city: "Seattle", market_county: "King", market_state: "WA", market_zip: "98107"})
+      puts FarMar::Market.all.length
+      FarMar::Market.all.length.must_equal(500)
+    end
+  end
+
 end
