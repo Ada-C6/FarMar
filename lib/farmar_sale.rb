c9e0c833da91f80d720c83541b468037952922a1
Esther Ng <esther.hc.ng@gmail.com>
2016-09-06 16:04:51 -0700

    Completed Sale all and find class methods and associated specs

--- lib/farmar_sale.rb
+++ lib/farmar_sale.rb
@@ -1,5 +1,30 @@
 module FarMar
   class Sale
+    attr_reader :id, :amount
 
+    def initialize(id, amount, purchase_time, vendor_id, product_id)
+      @id = id.to_i
+      @amount = amount.to_i
+      @purchase_time = purchase_time
+      @vendor_id = vendor_id.to_i
+      @product_id = product_id.to_i
+    end
+
+    def self.all
+      all_sales = []
+      CSV.read('support/sales.csv').each do |line|
+        all_sales << FarMar::Sale.new(line[0], line[1], line[2], line[3], line[4])
+      end
+      return all_sales
+    end
+
+    def self.find(id)
+      all_sales = self.all
+      all_sales.each do |sale|
+        if sale.id == id
+          return sale
+        end
+      end
+    end
   end
 end
--- specs/farmar_sale_spec.rb
+++ specs/farmar_sale_spec.rb
@@ -2,9 +2,44 @@ require_relative 'spec_helper'
 
 describe FarMar::Sale do
   describe "#initialize" do
-    sale = FarMar::Sale.new
+    sale = FarMar::Sale.new(9, 500, Time.now, 4, 100)
+
     it "should create an instance of FarMar::Sale class" do
       sale.must_be_instance_of(FarMar::Sale)
     end
+
+    it "should pass 5 arguments: ID - (Fixnum), Amount - (Fixnum), Purchase_time - (Datetime), Vendor_id - (Fixnum), Product_id - (Fixnum) and allow reading of ID" do
+      sale.id.must_equal(9)
+    end
+
+  end
+
+  describe "self.all" do
+    it "should return an array" do
+      FarMar::Sale.all.must_be_kind_of(Array)
+    end
+
+    it "should return an array of FarMar::Sale instances" do
+      FarMar::Sale.all[8].must_be_instance_of(FarMar::Sale)
+    end
+
+    it "should return a collection of FarMar::Sale instances created from the CSV file" do
+      FarMar::Sale.all[6].amount.must_equal(4095)
+    end
+  end
+
+  describe "self.find(id)" do
+    it "should return an instance of FarMar::Sale" do
+      FarMar::Sale.find(12).must_be_instance_of(FarMar::Sale)
+    end
+
+    it "should return the instance of FarMar::Sale that matches the passed id" do
+      FarMar::Sale.find(12).id.must_equal(12)
+    end
+
+    it "should return the instance of FarMar::Sale that matches the passed id, check for amount" do
+      FarMar::Sale.find(127).amount.must_equal(5790)
+    end
+
   end
 end
