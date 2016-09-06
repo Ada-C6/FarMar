3e7d7328f14a8574c5756a075ea2c7f75a25d51d
Kelly Tran <trankphuong@gmail.com>
2016-09-06 14:32:30 -0700

    Created self.all and self.find(id) in 4 data classes

--- lib/farmar_market.rb
+++ lib/farmar_market.rb
@@ -13,5 +13,26 @@ module FarMar
             @state = state # ((String) state in which the market is located
             @zip = zip # (String) zipcode in which the market is located
         end
+
+        def self.all
+            market_list = []
+
+            market_file = CSV.read('../support/markets.csv', 'r')
+            market_file.each do |line|
+                market_list << self.new(line[0].to_i, line[1], line[2], line[3], line[4], line[5], line[6])
+            end
+
+            return market_list
+        end
+
+        def self.find(id)
+            self.all.each do |item|
+                if item.id == id
+                    return item
+                end
+            end
+            puts "Sorry, invalid Market ID"
+            return nil
+        end
     end
 end
--- lib/farmar_product.rb
+++ lib/farmar_product.rb
@@ -1,11 +1,34 @@
 require 'csv'
 
-class FarMar::Product
-    attr_reader :id, :name, :vendor_id
+module FarMar
+    class Product
+        attr_reader :id, :name, :vendor_id
 
-    def initialize(id, name, vendor_id)
-        @id = id #(Fixnum) uniquely identifies the product
-        @name = name #(String) the name of the product (not guaranteed unique)
-        @vendor_id = vendor_id #(Fixnum) a reference to which vendor sells this product
+        def initialize(id, name, vendor_id)
+            @id = id #(Fixnum) uniquely identifies the product
+            @name = name #(String) the name of the product (not guaranteed unique)
+            @vendor_id = vendor_id #(Fixnum) a reference to which vendor sells this product
+        end
+
+        def self.all
+            product_list = []
+
+            product_file = CSV.read('../support/products.csv', 'r')
+            product_file.each do |line|
+                product_list << self.new(line[0].to_i, line[1], line[2].to_i)
+            end
+
+            return product_list
+        end
+
+        def self.find(id)
+            self.all.each do |item|
+                if item.id == id
+                    return item
+                end
+            end
+            puts "Sorry, invalid Product ID"
+            return nil
+        end
     end
 end
