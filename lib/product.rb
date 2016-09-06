a21dd9b6e05f540a41f8f55bcaf3ed8a2123d671
Madeleine Johnson <mjohnson3038@gmail.com>
2016-09-06 14:51:06 -0700

    class methods for Product complete

--- lib/product.rb
+++ lib/product.rb
@@ -1,6 +1,38 @@
 module FarMar
   class Product
+    attr_accessor :id, :name, :vendor_id
 
+    def initialize(id, name, vendor_id)
+      @id = id
+      @name = name
+      @vendor_id = vendor_id
+    end
 
+    def self.all
+      pdcts = []
+      CSV.read('/Users/madeleinejohnson/Ada/project-forks/FarMar/support/products.csv','r'). each do |line|
+        pdcts << FarMar::Product.new(line[0], line[1], line[2])
+      end
+      return pdcts
+    end
+
+    def self.find(id)
+      FarMar::Product.all.each do |line|
+        if id == line.id.to_i
+          return line
+        end
+      end
+    end
   end
 end
+
+
+# def self.find(id)
+#   FarMar::Market.all.each do |line|
+#     if id == line.id.to_i
+#       return line
+#     end
+#   end
+# end
+# end
+# end
--- specs/product_spec.rb
+++ specs/product_spec.rb
@@ -1,5 +1,24 @@
 require_relative 'spec_helper'
+require 'csv'
 
 describe FarMar::Product do
+  describe "#initialize" do
+    it "can initialize a new instance of Product" do
+      market = FarMar::Product.new("1","Dry Beets","1")
+      market.must_be_instance_of(FarMar::Product)
+    end
+  end
 
+  describe "#self.all" do
+    it "returns a collection of instances, representing CSV" do
+      FarMar::Product.all.length.must_equal(8193)
+    end
+  end
+
+  describe "#self.find(id)" do
+    it "should return an instance where the id matches the CSV field" do
+      FarMar::Product.all
+      FarMar::Product.find(8166).name.must_equal("Faithful Apples")
+    end
+  end
 end
