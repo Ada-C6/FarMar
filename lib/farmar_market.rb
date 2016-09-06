41177f99989a6d5213284ea9baacbf358e550a25
Shari Meggs <smeggs1@gmail.com>
2016-09-06 14:47:39 -0700

    updated the file with two methods self.all and self.find. Both are functional

--- lib/farmar_market.rb
+++ lib/farmar_market.rb
@@ -2,7 +2,7 @@ require 'csv'
 
 module FarMar
   class Market
-    attr_reader :id
+    attr_reader :id, :name, :address, :city, :state
   def initialize (id, name, address, city, county, state, zip)
     @id = id
     @name = name
@@ -12,5 +12,30 @@ module FarMar
     @state = state
     @zip = zip
   end
+
+def self.all
+  markets = {}
+  farm_mrkt = []
+  CSV.open('support/markets.csv', 'r'). each do |place|
+    markets[place[0]] = place[1..6]
+    id = place[0].to_i
+    name = place[1]
+    address = place[2]
+    city = place[3]
+    county = place[4]
+    state = place[5]
+    zip = place[6]
+    farm_mrkt << self.new(id, name, address, city, county, state, zip)
+  end
+  return farm_mrkt
+end
+def self.find(id)
+  id_find = self.all
+  id_find.each do |i|
+    if i.id == id
+      return i
+    end
+  end
+end
   end
 end
