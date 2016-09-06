206448083fa92011187a8a5dbd9188c996c0c10a
Esther Ng <esther.hc.ng@gmail.com>
2016-09-06 14:54:51 -0700

    Completed Vendor all and find class methods and associated specs

--- lib/farmar_vendor.rb
+++ lib/farmar_vendor.rb
@@ -1,5 +1,30 @@
 module FarMar
   class Vendor
+    attr_reader :id, :name
+
+    def initialize(id, name, employee_count, market_id)
+      @id = id.to_i
+      @name = name
+      @employee_count = employee_count.to_i
+      @market_id = market_id.to_i
+    end
+
+    def self.all
+      all_vendors = []
+      CSV.read('support/vendors.csv').each do |line|
+        all_vendors << FarMar::Vendor.new(line[0], line[1], line[2], line[3])
+      end
+      return all_vendors
+    end
+
+    def self.find(id)
+      all_vendors = self.all
+      all_vendors.each do |vendor|
+        if vendor.id == id
+          return vendor
+        end
+      end
+    end
 
   end
 end
--- specs/farmar_vendor_spec.rb
+++ specs/farmar_vendor_spec.rb
@@ -2,9 +2,43 @@ require_relative 'spec_helper'
 
 describe FarMar::Vendor do
   describe "#initialize" do
-    vendor = FarMar::Vendor.new
+    vendor = FarMar::Vendor.new(8, "Kool Kumquats", 3, 7)
+
     it "should create an instance of FarMar::Vendor class" do
       vendor.must_be_instance_of(FarMar::Vendor)
     end
+
+    it "should pass 4 arguments: ID - (Fixnum), Name - (String), No. of Employees - (Fixnum), Market_id - (Fixnum) and allow reading of ID" do
+      vendor.id.must_equal(8)
+    end
+  end
+
+  describe "self.all" do
+    it "should return an array" do
+      FarMar::Vendor.all.must_be_kind_of(Array)
+    end
+
+    it "should return an array of FarMar::Vendor instances" do
+      FarMar::Vendor.all[8].must_be_instance_of(FarMar::Vendor)
+    end
+
+    it "should return a collection of FarMar::Vendor instances created from the CSV file" do
+      FarMar::Vendor.all[6].name.must_equal("Bechtelar Inc")
+    end
+  end
+
+  describe "self.find(id)" do
+    it "should return an instance of FarMar::Vendor" do
+      FarMar::Vendor.find(12).must_be_instance_of(FarMar::Vendor)
+    end
+
+    it "should return the instance of FarMar::Vendor that matches the passed id" do
+      FarMar::Vendor.find(12).id.must_equal(12)
+    end
+
+    it "should return the instance of FarMar::Vendor that matches the passed id, check for name" do
+      FarMar::Vendor.find(12).name.must_equal("Windler Inc")
+    end
+
   end
 end
