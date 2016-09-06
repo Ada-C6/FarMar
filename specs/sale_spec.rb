dccd6be79071699f29d95e25513292272799d441
Karin Kubischta <karin.kubischta@gmail.com>
2016-09-06 11:40:09 -0700

    updated sale_spec and sale class

---
+++ specs/sale_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../lib/farmar_sale'
+require_relative 'spec_helper'
+
+module FarMar
+  describe Sale do
+    describe "#initialize" do
+      it "should make a new instance of sale" do
+        #write something here to test new instance of sale
+      end
+    end
+  end
+end
