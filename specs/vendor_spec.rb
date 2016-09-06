a7e76e673eb4e62ba1439d16f3f83522f2e307a7
Alma Maizels <aymaiz@yahoo.com>
2016-09-06 11:49:13 -0700

    Correction of baseline reqs

---
+++ specs/vendor_spec.rb
@@ -0,0 +1,21 @@
+require_relative "../lib/vendor.rb"
+require_relative 'spec_helper'
+module FarMar
+
+  describe Vendor do
+    # describe "#firstmethodprobably init" do
+    #   let(:market) { Market.new(:hash_maybe) }
+    #
+    #   it "jskdsjdksd" do
+    #
+    #
+    #   end
+
+
+    # end
+
+
+  end
+
+
+end
