0fbf0a6b333cffa40709709d465f6577335f2969
Shari Meggs <smeggs1@gmail.com>
2016-09-07 13:59:48 -0700

    Updated tests and code to have functionality to pass market_id and return vendor

--- lib/farmar_vendor.rb
+++ lib/farmar_vendor.rb
@@ -1,8 +1,40 @@
 class FarMar::Vendor
+  attr_reader :vend_id, :name, :num, :market_id
 def initialize(id, name, num, market_id)
-  @market_id = id
+  @vend_id = id
   @name = name
   @empl_num = num
   @market_id = market_id
+end
+
+def self.all
+  vendors = {}
+  farm_vend = []
+  CSV.open('support/vendors.csv', 'r').each do |vend|
+    vendors[vend[0]] = vend[1..3]
+    id = vend[0].to_i
+    name = vend[1]
+    num = vend[2]
+    market_id = vend[3].to_i
+    farm_vend << self.new(id, name, num, market_id)
+  end
+  return farm_vend
+end
+
+def self.find(id)
+  find_id = self.all
+  find_id.each do |i|
+    if i.vend_id == id
+      return i
+    end
+  end
+end
+def self.by_market(market_id)
+
+  vend_search = self.all
+  vend_search.find_all do |i|
+   i.market_id == market_id
+  end
+
 end
 end
--- specs/vendor_spec.rb
+++ specs/vendor_spec.rb
@@ -7,4 +7,22 @@ describe FarMar::Vendor do
       sam.must_be_instance_of(FarMar::Vendor)
     end
   end
+  describe "self.all" do
+    it "should return an array" do
+      FarMar::Vendor.all.must_be_kind_of(Array)
+
+    end
+  end
+  describe "self.find" do
+    it "should return the id that was passed through" do
+
+      FarMar::Vendor.find(1).vend_id.must_equal(1)
+    end
+  end
+  describe "self.by_market" do
+    it "should return vendors with a given market id" do
+    
+      FarMar::Vendor.by_market(1).must_be_kind_of(Array)
+    end
+  end
 end
