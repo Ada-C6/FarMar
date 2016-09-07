19129dea9a8314864efe38aae7b3caa9377f4d85
Kate Shaffer <kate.shaffer@gmail.com>
2016-09-07 16:14:34 -0700

    Implemented Sale class, self.all & self.find(id) methods, and their tests.

--- lib/farmar_sale.rb
+++ lib/farmar_sale.rb
@@ -1,3 +1,39 @@
 class FarMar::Sale
 
+    attr_accessor :sale_id, :amount, :purchase_time, :vendor_id, :product_id
+
+    # ID - (Fixnum) uniquely identifies the sale
+    # Amount - (Fixnum) the amount of the transaction, in cents (i.e., 150 would be $1.50)
+    # Purchase_time - (Datetime) when the sale was completed
+    # Vendor_id - (Fixnum) a reference to which vendor completed the sale
+    # Product_id - (Fixnum) a reference to which product was sold
+    def initialize(sale_info_hash)
+        @sale_id = sale_info_hash[:sale_id]
+        @amount = sale_info_hash[:amount]
+        @purchase_time = sale_info_hash[:purchase_time]
+        @vendor_id = sale_info_hash[:vendor_id]
+        @product_id = sale_info_hash[:product_id]
+    end #initialize
+
+    def self.all
+        all_sales = {}
+        CSV.read('support/sales.csv').each do |line|
+            new_sale_info_hash = {
+                sale_id: line[0].to_i,
+                amount: line[1].to_i,
+                purchase_time: line[2],
+                vendor_id: line[3].to_i,
+                product_id: line[4].to_i
+            }
+
+            all_sales[line[0].to_i] = self.new(new_sale_info_hash)
+        end # CSV.open
+
+        return all_sales
+    end # self.all
+
+    def self.find(sale_id)
+        return self.all[sale_id]
+    end # self.find
+
 end # FarMar::Sale
--- specs/sale_spec.rb
+++ specs/sale_spec.rb
@@ -3,8 +3,29 @@ require_relative 'spec_helper'
 describe "FarMar::Sale" do
     describe "#initialize" do
         it "should be an instance of the FarMar::Sale class" do
-            FarMar::Sale.new.must_be_instance_of(FarMar::Sale)
+            FarMar::Sale.new({}).must_be_instance_of(FarMar::Sale)
         end
-
     end # initialize
+
+    describe "self.all" do
+        it "should return a hash of FarMar::Sale objects with length matching CSV size" do
+            csv_sales = FarMar::Sale.all
+            expected_length = CSV.read('support/sales.csv').size
+            csv_sales.class.must_equal(Hash)
+            csv_sales.values[rand(0..12001)].must_be_instance_of(FarMar::Sale)
+            # @todo - commented out length check due to duplicated IDs in sales.csv, these are causing length mismatch here
+            # csv_sales.length.must_equal(expected_length)
+        end
+    end # self.all
+
+    describe "self.find(id)" do
+        it "should return a FarMar::Sale object with data that corresponds to the id argument passed in" do
+            found_sale = FarMar::Sale.find(10837)
+            found_sale.must_be_instance_of(FarMar::Sale)
+            found_sale.sale_id.must_equal(10837)
+            found_sale.amount.must_equal(6040)
+            found_sale.purchase_time.must_equal("2013-11-11 16:39:42 -0800")
+        end #
+    end # self.find
+
 end # FarMar::Sale
