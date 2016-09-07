ce868ce3b1f2fce1bac88349b8dfde9e79ae76c1
Jennifer Owens <jmo.jennifer@gmail.com>
2016-09-06 18:12:53 -0700

    Market self.find(id) method written with TDD spec passed; removed unnecessary CSV read instance variable in initialize

--- lib/farmar_market/farmar_market.rb
+++ lib/farmar_market/farmar_market.rb
@@ -4,29 +4,45 @@ require 'csv'
 module FarMar
   class Market
 
-    attr_reader :market_csv, :market_id, :market_name, :market_street, :market_city, :market_county, :market_state, :market_zip
+    attr_reader :market_id, :market_name, :market_street, :market_city, :market_county, :market_state, :market_zip
 
     def initialize(market_hash)
-      @market_csv = CSV.read("../FarMar/support/markets.csv", "r")
+      @market_id = market_hash[:market_id]
+      @market_name = market_hash[:market_name]
+      @market_street = market_hash[:market_street]
+      @market_city = market_hash[:market_city]
+      @market_county = market_hash[:market_county]
+      @market_state = market_hash[:market_state]
+      @market_zip = market_hash[:market_zip]
     end
 
     def self.all
-      market_csv_file = CSV.read("../FarMar/support/markets.csv", "r")
+      market_csv_file = CSV.read("../FarMar/support/markets.csv")
       market_array = []
-      market_hash = {}
 
       market_csv_file.each do |market|
-        market_hash[:id] = market[0].to_i
-        market_hash[:name] = market[1]
-        market_hash[:street] = market[2]
-        market_hash[:city] = market[3]
-        market_hash[:market_county] = market[4]
-        market_hash[:state] = market[5]
-        market_hash[:zip] = market[6]
-        market_array.push(self.new(market_hash))
+        market_hash = {}
+        market_hash[:market_id] = market[0].to_i
+        market_hash[:market_name] = market[1].to_s
+        market_hash[:market_street] = market[2].to_s
+        market_hash[:market_city] = market[3].to_s
+        market_hash[:market_county] = market[4].to_s
+        market_hash[:market_state] = market[5].to_s
+        market_hash[:market_zip] = market[6].to_i
+        market_array << self.new(market_hash)
       end
-
       return market_array
     end
+
+    def self.find(id)
+      markets = self.all
+      markets.each do |market|
+        if market.market_id == id.to_i
+          return market
+        end
+      end
+      raise Exception("ID was not present")
+    end
+
   end
 end
--- specs/farmar_market_spec.rb
+++ specs/farmar_market_spec.rb
@@ -2,25 +2,34 @@ require_relative 'spec_helper'
 
 describe FarMar::Market do
 
-  let(:market) { FarMar::Market.new({market_id: 6, market_name: "Test", market_street: "12345 Test Lane", market_city: "Seattle", market_county: "King", market_state: "WA", market_zip: "98107"}) }
+  let(:market) { FarMar::Market.new({market_id: 6, market_name: "Jefferson City Farmer's Market", market_street: "000 Main Street", market_city: "Jefferson City", market_county: "Cole", market_state: "Missouri", market_zip: 0}) }
 
   describe "#initialize" do
-
     it "can create a new instance of Market" do
       market.must_be_instance_of(FarMar::Market)
-    end
-
-    it "should know about its related CSV file" do
-      market.market_csv.wont_be_empty
     end
   end
 
   describe "all" do
     it "should put all values of its CSV file into an array" do
-      FarMar::Market.new({market_id: 6, market_name: "Test", market_street: "12345 Test Lane", market_city: "Seattle", market_county: "King", market_state: "WA", market_zip: "98107"})
-      puts FarMar::Market.all.length
       FarMar::Market.all.length.must_equal(500)
     end
+  end
+
+  describe "find(id)" do
+    it "should let the user know if the id is not present" do
+      proc { FarMar::Market.find(501) }.must_raise("ID was not present")
+    end
+
+    it "should find a specific Market by the market_id" do
+      FarMar::Market.all
+      found_market = FarMar::Market.find(6)
+      found_market.market_id.must_equal(6)
+      found_market.market_name.must_equal(market.market_name)
+    end
+
+
+
   end
 
 end
