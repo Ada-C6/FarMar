2f9ed1a5f1bd66a41ed6c8b32c0fb577f1818845
Allison Hoke <allisonhoke@gmail.com>
2016-09-07 16:48:14 -0700

    adjusted test for vendors method due to addition of class method in Vendor that does the same job - new # of arguments

--- specs/market_spec.rb
+++ specs/market_spec.rb
@@ -33,14 +33,14 @@ module FarMar
     end
 
     describe "#vendors" do
-      let(:example_market) { Market.new({}) }
+      let(:example_market) { Market.new({id: 4, name: "Allison's Market", address: "address", city: "city", county: "county", state: "state", zip: "zip"}) }
 
       it "returns an array" do
-        example_market.vendors(4).must_be_kind_of(Array)
+        example_market.vendors.must_be_kind_of(Array)
       end
 
       it "should return an object: Vendor at any index of the array" do
-        four = example_market.vendors(4)
+        four = example_market.vendors
 
         four[0].must_be_instance_of(Vendor)
         four[1].must_be_instance_of(Vendor)
@@ -48,7 +48,7 @@ module FarMar
       end
 
       it "should return the correct market_id that matches the one searched" do
-        four = example_market.vendors(4)
+        four = example_market.vendors
 
         four[0].market_id.must_equal(4)
         four[1].market_id.must_equal(4)
