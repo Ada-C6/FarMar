d91ef66e13e37dc8b9c46f4c7a8b269757f0575e
Natasha Ewing <natasharai@me.com>
2016-09-07 14:56:08 -0700

    "instance methods all and find working for market"

--- lib/mkt.rb
+++ lib/mkt.rb
@@ -3,37 +3,32 @@ require 'csv'
 module FarMar
 
   class Market
-    attr_accessor :id, :name, :city
+    attr_accessor :id, :name, :street, :city, :county, :state, :zip
     def initialize(id, name, street, city, county, state, zip)
-      @id = id
-      @name = name
-      @street = street
-      @city = city
-      @county = county
-      @state = state
-      @zip = zip
-      @all = all
-
-    end
-
-    def compile
-      @all = {:id => @id, :name => @name, :street => @street , :city => @city, :county => @county, :state => @state, :zip => @zip}
-        return @all
+      self.id = id
+      self.name = name
+      self.street = street
+      self.city = city
+      self.county = county
+      self.state = state
+      self.zip = zip
+      #self.instances = instances
     end
 
     def self.all
-      mkt =  {}
-
+      mkt= { }
         CSV.read('../support/markets.csv').each do |line|
-        mkt[line[0]] = line[1..line.length]
-          puts mkt
-      end
-        return mkt
+          mkt[line[0]] = Market.new(line[0],line[1],line[2],line[3], line[4], line[5], line[6])
+        end
+      return mkt
+    end
+
+    def self.find(id)
+        Market.all[id] 
     end
   end
-
-
 end
-#FarMar::Market.new("9999999", "Natasha", "737 Olive Way", "Seattle", "King", "WA", "98101")
 
-  puts FarMar::Market.all
+#natasha = FarMar::Market.new("9999999","Natasha","737 Olive Way","Seattle","King","WA","98101")
+puts FarMar::Market.all
+puts FarMar::Market.find("492")
