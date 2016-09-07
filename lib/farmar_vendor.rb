02e99f72287c0a867d53b34fc6150d2410a84a7c
Kate Shaffer <kate.shaffer@gmail.com>
2016-09-07 15:56:09 -0700

    Implemented Vendor class initialize, self.all, & self.find(id) methods & tests

--- lib/farmar_vendor.rb
+++ lib/farmar_vendor.rb
@@ -1,3 +1,36 @@
 class FarMar::Vendor
 
+    attr_accessor :vendor_id, :name, :employees, :market_id
+
+    # ID - (Fixnum) uniquely identifies the vendor
+    # Name - (String) the name of the vendor (not guaranteed unique)
+    # No. of Employees - (Fixnum) How many employees the vendor has at the market
+    # Market_id - (Fixnum) a reference to which market the vendor attends
+    def initialize(vendor_info_hash)
+        @vendor_id = vendor_info_hash[:vendor_id]
+        @name = vendor_info_hash[:name]
+        @employees = vendor_info_hash[:employees]
+        @market_id = vendor_info_hash[:market_id]
+    end #initialize
+
+    def self.all
+        all_vendors = {}
+        CSV.read('support/vendors.csv').each do |line|
+            new_vendor_info_hash = {
+                vendor_id: line[0].to_i,
+                name: line[1],
+                employees: line[2],
+                market_id: line[3]
+            }
+
+            all_vendors[line[0].to_i] = self.new(new_vendor_info_hash)
+        end # CSV.open
+
+        return all_vendors
+    end # self.all
+
+    def self.find(vendor_id)
+        return self.all[vendor_id]
+    end # self.find
+
 end # FarMar::Vendor
--- specs/vendor_spec.rb
+++ specs/vendor_spec.rb
@@ -3,8 +3,27 @@ require_relative 'spec_helper'
 describe "FarMar::Vendor" do
     describe "#initialize" do
         it "should be an instance of the FarMar::Vendor class" do
-            FarMar::Vendor.new.must_be_instance_of(FarMar::Vendor)
+            FarMar::Vendor.new({}).must_be_instance_of(FarMar::Vendor)
         end
-
     end # initialize
+
+    describe "self.all" do
+        it "should return a hash of FarMar::Vendor objects with length matching CSV size" do
+            csv_vendors = FarMar::Vendor.all
+            expected_length = CSV.read('support/vendors.csv').size
+            csv_vendors.class.must_equal(Hash)
+            csv_vendors.values[rand(0..expected_length)].must_be_instance_of(FarMar::Vendor)
+            csv_vendors.length.must_equal(expected_length)
+        end
+    end # self.all
+
+    describe "self.find(id)" do
+        it "should return a FarMar::Vendor object with data that corresponds to the id argument passed in" do
+            found_vendor = FarMar::Vendor.find(2468)
+            found_vendor.must_be_instance_of(FarMar::Vendor)
+            found_vendor.vendor_id.must_equal(2468)
+            found_vendor.name.must_equal("Tillman-Schowalter")
+        end #
+    end # self.find
+
 end # FarMar::Vendor
