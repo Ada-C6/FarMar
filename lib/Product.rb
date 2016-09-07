bb7c204082de234b0217728a4033ec671295ca5d
Dianne Laguerta <diannelaguerta@gmail.com>
2016-09-07 14:34:04 -0700

    added spec and method for initializing Product instances, passed

--- lib/Product.rb
+++ lib/Product.rb
@@ -2,5 +2,13 @@ require 'CSV'
 
 module FarMar
   class Product
+    attr_reader :id, :name, :vendor_id
+
+    def initialize(products_hash)
+      @id = products_hash[:id]
+      @name = products_hash[:name]
+      @vendor_id = products_hash[:vendor_id]
+
+    end
   end
 end
--- specs/product_spec.rb
+++ specs/product_spec.rb
@@ -1,5 +1,19 @@
 require_relative 'spec_helper'
-# module FarMar
-#  describe Product do
-#  end
-# end
+module FarMar
+ describe Product do
+   describe "#initialize" do
+     my_hash = {
+       id:  23,
+       name: "Ada Farms Milk",
+       vendor_id: 112
+     }
+
+     let(:product) { Product.new(my_hash) }
+     it "should create an instance of a product" do
+       product.must_be_instance_of(Product)
+     end
+
+   end #end initialize method
+
+ end #end Product
+end #end module
