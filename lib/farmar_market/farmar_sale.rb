9fc85f03bbbf1fed7a9325a89ae9aa32c629131d
Jennifer Owens <jmo.jennifer@gmail.com>
2016-09-06 22:57:18 -0700

    WIP Sale self.all, self.find(id) and tests. Data cleanup (sale_id), time display required

--- lib/farmar_market/farmar_sale.rb
+++ lib/farmar_market/farmar_sale.rb
@@ -4,10 +4,44 @@ require 'csv'
 module FarMar
   class Sale
 
-    attr_reader :sale_csv
+    attr_reader :sale_id, :sale_amount, :sale_purchase_time, :vendor_id, :product_id, :sale_state, :sale_zip
 
-    def initialize
-      
+    def initialize(sale_hash)
+      @sale_id = sale_hash[:sale_id]
+      @sale_amount = sale_hash[:sale_amount]
+      @sale_purchase_time = sale_hash[:sale_purchase_time]
+      @vendor_id = sale_hash[:vendor_id]
+      @product_id = sale_hash[:product_id]
+
+    end
+
+    def self.all
+      sale_csv_file = CSV.read("../FarMar/support/sales.csv")
+      sale_array = []
+
+      sale_csv_file.each do |sale|
+        sale_hash = {}
+        # Will need to determine data clean-up steps--add 797 to second listed set of ids, and make 00 be 12002? What about nil values?
+        sale_hash[:sale_id] = sale[0].to_i
+        sale_hash[:sale_amount] = sale[1].to_i
+        # Will need to determine how to format time data into non-String
+        sale_hash[:sale_purchase_time] = sale[2].to_s
+        sale_hash[:vendor_id] = sale[3].to_i
+        sale_hash[:product_id] = sale[4].to_i
+
+        sale_array << self.new(sale_hash)
+      end
+      return sale_array
+    end
+
+    def self.find(id)
+      sales = self.all
+      sales.each do |sale|
+        if sale.sale_id == id.to_i
+          return sale
+        end
+      end
+      raise Exception("ID was not present")
     end
 
   end
--- specs/farmar_sale_spec.rb
+++ specs/farmar_sale_spec.rb
@@ -2,13 +2,32 @@ require_relative 'spec_helper'
 
 describe FarMar::Sale do
 
-  let(:sale) { FarMar::Sale.new }
+  # Will need to determine how to format time data into non-String
+  # Will need to determine data clean-up steps--add 797 to second listed set of ids, and make 00 be 12002?
+  let(:sale) { FarMar::Sale.new({sale_id: 1, sale_amount: 9290, purchase_time: "2013-11-07 04:34:56 -0800", vendor_id: 1, product_id: 1, market_zip: 0}) }
 
   describe "#initialize" do
-
     it "can create a new instance of Sale" do
       sale.must_be_instance_of(FarMar::Sale)
     end
-    
+  end
+
+  describe "all" do
+    it "should put all values of its CSV file into an array" do
+      FarMar::Sale.all.length.must_equal(12798)
+    end
+  end
+
+  describe "find(id)" do
+    it "should let the user know if the id is not present" do
+      proc { FarMar::Sale.find(12002) }.must_raise("ID was not present")
+    end
+
+    it "should find a specific Sale by the sale_id" do
+      FarMar::Sale.all
+      found_sale = FarMar::Sale.find(1)
+      found_sale.sale_id.must_equal(1)
+      found_sale.sale_amount.must_equal(sale.sale_amount)
+    end
   end
 end
