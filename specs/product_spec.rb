b16954ca6bfe203b48eeb47dcafd6af0a2dd0976
Alma Maizels <aymaiz@yahoo.com>
2016-09-06 22:58:54 -0700

    Fixed trouble Product class. All classes have .all and .self that pass tests

--- specs/product_spec.rb
+++ specs/product_spec.rb
@@ -36,14 +36,13 @@ module FarMar
             Product.all[8193].vendor_id.must_equal(2690)
           end
         end #self.all
-#@todo START HERE START HERE START HERE START HERE
-        # describe "#self.find(id)" do
-        #   it "should return an instance of a Product object of a certain id" do
-        #     random_product_id = rand(1..8193).to_s
-        #     Product.find(random_product_id).must_be_instance_of(Product)
-        #     Product.find(random_product_id).id.must_equal(random_product_id)
-        #   end
-        # end #self.find(id)
+        describe "#self.find(id)" do
+          it "should return an instance of a Product object of a certain id" do
+            random_product_id = rand(1..8193)
+            Product.find(random_product_id).must_be_instance_of(Product)
+            Product.find(random_product_id).id.must_equal(random_product_id)
+          end
+        end #self.find(id)
 
 
 
