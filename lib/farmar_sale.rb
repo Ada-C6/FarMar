42754dcd94fd7952cfe9e7e0ded5e1e1f02c2cb0
Karin Kubischta <karin.kubischta@gmail.com>
2016-09-06 17:06:04 -0700

    added self.all spec, it's currently broken

--- lib/farmar_sale.rb
+++ lib/farmar_sale.rb
@@ -1,16 +1,41 @@
 require 'csv'
+require 'date'
 
 module FarMar
   class Sale
+    attr_reader :sale_id, :amount, :vendor_id, :product_id, :purchase_time
     def initialize(sale_id, amount, purchase_time, vendor_id, product_id)
+      # sale_id, amount, purchase_time, vendor_id, product_id)
       # ID - (Fixnum) uniquely identifies the sale
+      @sale_id = sale_id
       # Amount - (Fixnum) the amount of the transaction, in cents (i.e., 150 would be $1.50)
+      @amount = amount
       # Purchase_time - (Datetime) when the sale was completed
+      @purchase_time = DateTime.new(purchase_time) #BUG: purchase_time in csv is the string representation of a datetime, not the comma delimited input that Datetime is expecting. https://ruby-doc.org/stdlib-2.3.1/libdoc/date/rdoc/DateTime.html#method-c-parse 
       # Vendor_id - (Fixnum) a reference to which vendor completed the sale
+      @vendor_id = vendor_id
       # Product_id - (Fixnum) a reference to which product was sold
+      @product_id = product_id
+    end
+
+    def self.all
+      # self.all: returns a collection of instances, representing all of the objects described in the CSV
+      sales = {}
+
+      CSV.read("support/sales.csv").each do |line|
+        sale_id, amount, purchase_time, vendor_id, product_id = line # parallel assignment!
+        sale_id = sale_id.to_i # need sale_id to be a fixnum
+        amount = amount.to_i # want amount to be a fixnum also
+        purchase_time = DateTime.new(purchase_time) #BUG: the purchase_times are strings, that are the DateTime format, but not the format for making a new DateTime. This is currently breaking the .all test in sale_spec.
+        vendor_id = vendor_id # want vendor_id to be a fixnum
+        product_id = product_id # want product_id also to be a fixnum
+
+        sales[sale_id] = self.new(sale_id, amount, purchase_time, vendor_id, product_id)
+      end
+
+      return sales
     end
   end
 end
 
-# self.all: returns a collection of instances, representing all of the objects described in the CSV
 # self.find(id): returns an instance of the object where the value of the id field in the CSV matches the passed parameter.
--- specs/sale_spec.rb
+++ specs/sale_spec.rb
@@ -2,18 +2,34 @@ require_relative 'spec_helper'
 
 module FarMar
   describe Sale do
+    let(:buy_a_thing) {Sale.new(1,100,2016, 1, 1)}
     describe "#initialize" do
       it "should make a new instance of sale" do
         #write something here to test new instance of sale
-        Sale.new(1, 100, DateTime.new(2104), 1, 1).must_be_instance_of(Sale)
+        buy_a_thing.must_be_instance_of(Sale)
         # ID - (Fixnum) uniquely identifies the sale
         # Amount - (Fixnum) the amount of the transaction, in cents (i.e., 150 would be $1.50)
         # Purchase_time - (Datetime) when the sale was completed
         # Vendor_id - (Fixnum) a reference to which vendor completed the sale
         # Product_id - (Fixnum) a reference to which product was sold
       end
+
+      it "should have a sale amount" do
+        buy_a_thing.must_respond_to :amount
+      end
+
+      it "should have a purchase_time" do
+        buy_a_thing.purchase_time.must_be_instance_of(DateTime)
+      end
     end
-    # self.all: returns a collection of instances, representing all of the objects described in the CSV
+
+    describe "self.all" do
+      # self.all: returns a collection of instances, representing all of the objects described in the CSV
+      it "should return a collection of Sale objects" do
+        # I'm going to start with hash with sale ID as the key. I think I'm going to need to get back out the vendor and product associated with it, but this seems good for now.
+        Sale.all.must_be_instance_of(Hash)
+      end
+    end
     # self.find(id): returns an instance of the object where the value of the id field in the CSV matches the passed parameter.
   end
 end
