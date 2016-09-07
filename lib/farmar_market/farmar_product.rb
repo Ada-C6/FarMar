54e400e3d780d5cdedb2b0b6aa0cf01144cd304c
Jennifer Owens <jmo.jennifer@gmail.com>
2016-09-06 18:20:17 -0700

    Market self.find(id) method written with TDD spec passed; removed unn…

--- lib/farmar_market/farmar_product.rb
+++ lib/farmar_market/farmar_product.rb
@@ -4,10 +4,37 @@ require 'csv'
 module FarMar
   class Product
 
-    attr_reader :product_csv
+    attr_reader :product_id, :product_name, :vendor_id
 
-    def initialize
-      @product_csv = CSV.read("../FarMar/support/products.csv", "r")
+    def initialize(product_hash)
+      @product_id = product_hash[:product_id]
+      @product_name = product_hash[:product_name]
+      @vendor_id = product_hash[:vendor_id]
+    end
+
+    def self.all
+      product_csv_file = CSV.read("../FarMar/support/products.csv")
+      product_array = []
+
+      product_csv_file.each do |market|
+        product_hash = {}
+        product_hash[:product_id] = market[0].to_i
+        product_hash[:product_name] = market[1].to_s
+        product_hash[:vendor_id] = market[2].to_i
+
+        product_array << self.new(product_hash)
+      end
+      return product_array
+    end
+
+    def self.find(id)
+      products = self.all
+      products.each do |product|
+        if product.product_id == id.to_i
+          return product
+        end
+      end
+      raise Exception("ID was not present")
     end
 
   end
--- specs/farmar_product_spec.rb
+++ specs/farmar_product_spec.rb
@@ -2,7 +2,7 @@ require_relative 'spec_helper'
 
 describe FarMar::Product do
 
-  let(:product) { FarMar::Product.new }
+  let(:product) { FarMar::Product.new( {product_id: 1, product_name: "Dry Beets",vendor_id: 1} ) }
 
   describe "#initialize" do
 
@@ -10,8 +10,24 @@ describe FarMar::Product do
       product.must_be_instance_of(FarMar::Product)
     end
 
-    it "should know about its related CSV file" do
-      product.product_csv.wont_be_empty
+  end
+
+  describe "all" do
+    it "should put all values of its CSV file into an array" do
+      FarMar::Product.all.length.must_equal(8193)
+    end
+  end
+
+  describe "find(id)" do
+    it "should let the user know if the id is not present" do
+      proc { FarMar::Product.find(8194) }.must_raise("ID was not present")
+    end
+
+    it "should find a specific Product by the market_id" do
+      FarMar::Product.all
+      found_product = FarMar::Product.find(1)
+      found_product.product_id.must_equal(1)
+      found_product.product_name.must_equal(product.product_name)
     end
   end
 end
