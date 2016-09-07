01293cdf38782ef59b2833072f19c71548f0862d
Kate Shaffer <kate.shaffer@gmail.com>
2016-09-06 17:01:05 -0700

    Implemented initialize, self.all, and self.find(id) methods & tests for Market class

--- lib/farmar_market.rb
+++ lib/farmar_market.rb
@@ -1,5 +1,44 @@
 class FarMar::Market
-    def initialize
-        
+
+    attr_accessor :id, :name, :address, :city, :county, :state, :zip
+    # ID - (Fixnum) a unique identifier for that market
+    # Name - (String) the name of the market (not guaranteed unique)
+    # Address - (String) street address of the market
+    # City - (String) city in which the market is located
+    # County - (String) county in which the market is located
+    # State - (String) state in which the market is located
+    # Zip - (String) zipcode in which the market is located
+    def initialize(market_info_hash)
+        @id = market_info_hash[:id]
+        @name = market_info_hash[:name]
+        @address = market_info_hash[:address]
+        @city = market_info_hash[:city]
+        @county = market_info_hash[:county]
+        @state = market_info_hash[:state]
+        @zip = market_info_hash[:zip]
     end #initialize
+
+    def self.all
+        all_markets = {}
+        CSV.read('support/markets.csv').each do |line|
+            new_market_info_hash = {
+                id: line[0].to_i,
+                name: line[1],
+                address: line[2],
+                city: line[3],
+                county: line[4],
+                state: line[5],
+                zip: line[6]
+            }
+
+            all_markets[line[0].to_i] = self.new(new_market_info_hash)
+        end # CSV.open
+
+        return all_markets
+    end # self.all
+
+    def self.find(id)
+        return self.all[id]
+    end # self.find
+
 end # FarMar::Market
--- specs/market_spec.rb
+++ specs/market_spec.rb
@@ -1,10 +1,31 @@
 require_relative 'spec_helper'
 
 describe "FarMar::Market" do
+
     describe "#initialize" do
         it "should be an instance of the FarMar::Market class" do
-            FarMar::Market.new.must_be_instance_of(FarMar::Market)
+            FarMar::Market.new({}).must_be_instance_of(FarMar::Market)
+        end
+    end # initialize
+
+    describe "self.all" do
+        it "should return a hash of FarMar::Market objects with quantity matching CSV length" do
+            csv_markets = FarMar::Market.all
+            csv_markets.class.must_equal(Hash)
+            csv_markets.values[rand(1..500)].must_be_instance_of(FarMar::Market)
+            expected_length = CSV.read('support/markets.csv').size
+            csv_markets.length.must_equal(expected_length)
         end
 
-    end # initialize
+    end # self.all
+
+    describe "self.find(id)" do
+        it "should return a FarMar::Market object with data that corresponds to the id argument passed in" do
+            found_market = FarMar::Market.find(126)
+            found_market.must_be_instance_of(FarMar::Market)
+            found_market.id.must_equal(126)
+            found_market.zip.must_equal("65536")
+        end #
+    end # self.find
+
 end # FarMar::Market
