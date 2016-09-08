df18ebc620732b9ea440ed79500b5a117c3df549
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-09-08 16:18:01 -0700

    Clean up unused commented tests

--- specs/farmar_sale_spec.rb
+++ specs/farmar_sale_spec.rb
@@ -1,5 +1,4 @@
 require_relative 'spec_helper'
-# require '../lib/farmar_sale'
 
 describe Sale do
   let s = Sale.new(12, 100, '2013-11-13 01:49:37 -0800', 3, 1)
@@ -7,21 +6,16 @@ describe Sale do
 
   describe "#initialize" do
     it "should create an instance of Sale" do
-      # time = DateTime.strptime('2013-11-13 01:49:37 -0800', '%Y-%m-%d %H:%M:%S %z')
-      # s = Sale.new(12, 100, '2013-11-13 01:49:37 -0800', 3, 1)
       s.must_be_instance_of(Sale)
     end
 
     it "should know about associated data file" do
-      # s = Sale.new(12, 100, '2013-11-13 01:49:37 -0800', 3, 1)
-      # s = Sale.all
       all.class.must_equal(Hash)
     end
   end
 
   describe "all" do
     it "should return a hash" do
-      # s = Sale.all
       all.class.must_equal(Hash)
     end
   end
