426bbe4b80d78c189b080f4a54af313a4e1be491
Yeni <yenicapote08@gmail.com>
2016-09-08 14:50:24 -0700

    added vendor, sales, number of sales, and self.by_vendor(vendor_id) methods and the specs.

--- lib/product.rb
+++ lib/product.rb
@@ -4,9 +4,9 @@ module FarMar
     attr_accessor :id, :name, :vendor_id
 
     def initialize(id, name, vendor_id)
-      @id = id
+      @id = id.to_i
       @name = name
-      @vendor_id = vendor_id
+      @vendor_id = vendor_id.to_i
     end
 
     def self.load_all
@@ -28,6 +28,47 @@ module FarMar
 
     def self.find(id)
       return self.all[id]
+    end
+
+    def vendor
+      this_vendor = FarMar::Vendor.find(@vendor_id)
+      return this_vendor
+    end
+
+    def sales
+      all_sales = FarMar::Sale.all
+      sales = []
+
+      all_sales.each do |var, sale|
+        if sale.product_id == @id
+          sales << sale
+        end
+      end
+      return sales
+    end
+
+    def number_of_sales
+      all_sales = FarMar::Sale.all
+      num_sales = 0
+
+      all_sales.each do |var, sale|
+        if sale.product_id == @id
+          num_sales += 1
+        end
+      end
+      return num_sales
+    end
+
+    def self.by_vendor(this_vendor_id)
+      all_products = FarMar::Product.all
+      products = []
+
+      all_products.each do |var, product|
+        if product.vendor_id == this_vendor_id
+          products << product
+        end
+      end
+      return products
     end
   end
 end
--- specs/product_spec.rb
+++ specs/product_spec.rb
@@ -24,4 +24,33 @@ describe FarMar::Product do
       fruit.id.must_equal(2)
     end
   end
+
+  describe "#vendor" do
+    it "should return the vendor instance associated with this product" do
+      green_apple = FarMar::Product.find(5)
+      fruit_vendor = FarMar::Vendor.find(4)
+      green_apple.vendor.must_equal(fruit_vendor)
+    end
+  end
+
+  describe "#sales" do
+    it "should return a collection of sales associated with this product" do
+      green_apple = FarMar::Product.find(5)
+      green_apple.sales.length.must_equal(2)
+    end
+  end
+
+  describe "#number_of_sales" do
+    it "should return the number of times the product has been sold" do
+      green_apple = FarMar::Product.find(5)
+      green_apple.number_of_sales.must_equal(2)
+    end
+  end
+
+  describe "self.by_vendor(vendor_id)" do
+    it "should return all of the products associated with that vendor id" do
+      veg_vendor = FarMar::Product.by_vendor(4)
+      veg_vendor.length.must_equal(3)
+    end
+  end
 end
