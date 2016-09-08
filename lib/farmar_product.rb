173fe53efa1e519e7d154448c7d39cc9a6fcd0a7
Shari Meggs <smeggs1@gmail.com>
2016-09-07 17:10:14 -0700

    added the by vendor method and assosicated tests

--- lib/farmar_product.rb
+++ lib/farmar_product.rb
@@ -1,9 +1,9 @@
 class FarMar::Product
-attr_reader :id
-  def initialize (id, name, vend)
+attr_reader :id, :name, :vend_id
+  def initialize (id, name, vend_id)
     @id = id
     @name = name
-    @vend_id = vend
+    @vend_id = vend_id
   end
   def self.all
     products = {}
@@ -12,7 +12,7 @@ attr_reader :id
       products[line[0]] = line[1..2]
       id = line[0].to_i
       name = line[1]
-      vend = line[2]
+      vend = line[2].to_i
       merch << self.new(id, name, vend)
     end
     return merch
@@ -24,6 +24,13 @@ attr_reader :id
       if i.id == id
         return i
       end
+    end
+  end
+
+  def self.by_vendor(vendor_id)
+    prod_search = self.all
+    prod_search.find_all do |i|
+      i.vend_id == vendor_id
     end
   end
 
--- specs/product_spec.rb
+++ specs/product_spec.rb
@@ -1,7 +1,7 @@
 require_relative 'spec_helper'
 
 describe FarMar::Product do
-
+let(:sam) {FarMar::Product.new(19, "Jane", 1)}
 describe "#initialize" do
   it "can create a new instance of Product" do
     sam = FarMar::Product.new(19, "Jane", 81)
@@ -16,6 +16,14 @@ end
 describe "self.find(id)" do
   it "should return the id that was passed through" do
     FarMar::Product.find(1).id.must_equal(1)
+  end
+end
+describe "self.by_vendor(vendor_id)" do
+  it "returns the vendors of a given vendor id" do
+    puts "#{FarMar::Product.by_vendor(1)}"
+    FarMar::Product.by_vendor(1).each do |x|
+      x.vend_id.must_equal(1)
+    end
   end
 end
 end
