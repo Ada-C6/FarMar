369ae40c9dac5a2b2bc7f46e41a9be9b39166b9b
Shari Meggs <smeggs1@gmail.com>
2016-09-08 13:42:43 -0700

    updated the let line to better test the value of an ID

--- specs/product_spec.rb
+++ specs/product_spec.rb
@@ -1,10 +1,9 @@
 require_relative 'spec_helper'
 
 describe FarMar::Product do
-let(:sam) {FarMar::Product.new(19, "Jane", 1)}
+let(:sam) {FarMar::Product.new(58, "Jane", 1)}
 describe "#initialize" do
   it "can create a new instance of Product" do
-    sam = FarMar::Product.new(19, "Jane", 81)
     sam.must_be_instance_of(FarMar::Product)
   end
 end
