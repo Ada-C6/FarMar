2726649d1223185b6d086b4fb85638c12f31c426
Allison Hoke <allisonhoke@gmail.com>
2016-09-08 14:13:10 -0700

    added product method and self.between method

--- lib/sale.rb
+++ lib/sale.rb
@@ -1,6 +1,6 @@
 module FarMar
   class Sale
-    attr_reader :id, :amount, :vendor_id, :product_id
+    attr_reader :id, :amount, :vendor_id, :product_id, :purchase_time
 
     def initialize(sale_hash)
       @id = sale_hash[:id]
@@ -15,7 +15,7 @@ module FarMar
       # IDEA make this an instance variable so you only have to make it run once
       sales = [] #array to store all of the hashes with sale info
       CSV.read("../FarMar/support/sales.csv").each do |line|
-        sale = {id: line[0].to_i, amount: line[1].to_i, purchase_time: Time.parse(line[2]), vendor_id: line[3].to_i, product_id: line[4].to_i } #create a new hash for each sale to store specific info
+        sale = {id: line[0].to_i, amount: line[1].to_i, purchase_time: DateTime.parse(line[2]), vendor_id: line[3].to_i, product_id: line[4].to_i } #create a new hash for each sale to store specific info
 
         sales << self.new(sale) #creates a new instance with the hash info and puts it in the array to be returned
       end
@@ -32,6 +32,21 @@ module FarMar
 
     def vendor #returns the Vendor instance that is associated with this sale
       FarMar::Vendor.find(@vendor_id)
+    end
+
+    def product
+      FarMar::Product.find(@product_id)
+    end
+
+    def self.between(beginning_time, end_time)
+      sales_between = []
+
+      self.all.each do |s|
+        if s.purchase_time >= beginning_time && s.purchase_time <= end_time
+          sales_between << s
+        end
+      end
+      return sales_between
     end
   end
 end
