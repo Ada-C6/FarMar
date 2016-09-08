23e964f57e705e4a64a32112f44267877aa0d7c2
Shari Meggs <smeggs1@gmail.com>
2016-09-08 16:27:31 -0700

    made two tests one made sure that the between method returned an array and the other checked that the method would not include dates outside the range

--- specs/sale_spec.rb
+++ specs/sale_spec.rb
@@ -16,7 +16,6 @@ describe FarMar::Sale do
   end
   describe "self.find(id)" do
     it "should return the id that was passed through" do
-
       FarMar::Sale.find(1).id.must_equal(1)
     end
  end
@@ -30,5 +29,12 @@ describe FarMar::Sale do
      sam.product.must_be_instance_of(FarMar::Product)
    end
  end
-
+describe "self.between(begin_time, end_time)" do
+  it "will return FarMar:Sale instances" do
+    FarMar::Sale.between("2013-11-11 11:29:52 -0800", "2013-11-12 14:38:29 -0800").must_be_kind_of(Array)
+  end
+  it "will not include dates outside of the range" do
+    FarMar::Sale.between("2013-11-11 11:29:52 -0800", "2013-11-12 14:38:29 -0800").wont_include("2013-11-13 01:48:15 -0800")
+  end
+end
 end
