edd455950d23ffcf7a5552290c68796b01764a76
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-09-08 14:42:29 -0700

    Refactor product specs to reuse Product instances.

--- specs/farmar_product_spec.rb
+++ specs/farmar_product_spec.rb
@@ -2,47 +2,47 @@ require_relative 'spec_helper'
 # require '../lib/farmar_product'
 
 describe Product do
+  let all = Product.all
+  let prod = Product.new(1,"Dry Beets",1)
+
 
   describe "#initialize" do
     it "should create an instance of Product" do
-      p = Product.new(123, "fruits", 12)
-      p.must_be_instance_of (Product)
+      # p = Product.new(123, "fruits", 12)
+      prod.must_be_instance_of(Product)
     end
 
     it "should know about associated data file" do
-      p = Product.all
-      p[4].name.must_equal("Yummy Fruit")
+      all[4].name.must_equal("Yummy Fruit")
     end
   end
 
   describe "all" do
     it "should return a hash" do
-      p = Product.all
-      p.class.must_equal(Hash)
+      all.class.must_equal(Hash)
     end
   end
 
   describe "find" do
     it "should return an instance of the object" do
-      p = Product.find(11)
-      p.id.must_equal(11)
-      p.must_be_instance_of(Product)
-      p.name.must_equal("Gigantic Bread")
+      finder = Product.find(11)
+      finder.id.must_equal(11)
+      finder.must_be_instance_of(Product)
+      finder.name.must_equal("Gigantic Bread")
     end
   end
 
   describe "#vendor" do
     it "should return the Vendor instance that is associated with the product vendor_id" do
-      p = Product.new(1,"Dry Beets",1).vendor
-      p.must_be_instance_of(Vendor)
-      p.name.must_equal("Feil-Farrell")
+      vend = prod.vendor
+      vend.must_be_instance_of(Vendor)
+      vend.name.must_equal("Feil-Farrell")
     end
   end
 
   describe "#number_of_sales" do
     it "should return the number of times this product has been sold" do
-      Product.new(1,"Dry Beets",1).number_of_sales.must_equal(7)
-
+      prod.number_of_sales.must_equal(7)
     end
   end
 
