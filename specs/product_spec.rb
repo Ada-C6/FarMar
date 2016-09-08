a82016ab0b5f6906ff56854bd6614a799fd24252
guineveresaenger <guineveresaenger@yahoo.com>
2016-09-08 11:38:55 -0700

    Cleaned up some of the tests

--- specs/product_spec.rb
+++ specs/product_spec.rb
@@ -21,7 +21,6 @@ describe FarMar::Product do
     end
 
     it "has a Product as its first element" do
-
       FarMar::Product.all[0].must_be_instance_of(FarMar::Product)
     end
 
@@ -29,19 +28,18 @@ describe FarMar::Product do
 
   describe "self.find" do
     it "returns a Market instance" do
-
-
       FarMar::Product.find(123).must_be_instance_of(FarMar::Product)
     end
   end
 
   describe "self.by_vendor" do
+    # I am assuming at this point that my code is tightly linked to the data
+    # files, so I feel OK writing a test that uses the actual data file.
+    # Not sure if that is good test writing.
     it "returns an array of Products" do
       FarMar::Product.by_vendor(37).must_be_instance_of Array
       FarMar::Product.by_vendor(37)[0].must_be_instance_of FarMar::Product
       FarMar::Product.by_vendor(34)[0].name.must_equal "Weissnat LLC"
-
-
     end
   end
 
@@ -50,7 +48,6 @@ describe FarMar::Product do
     it "returns the vendor given by product's vendor id" do
       new_product.vendor.id.must_equal 4
       new_product.vendor.must_be_instance_of FarMar::Vendor
-
     end
   end
 end
--- specs/vendor_spec.rb
+++ specs/vendor_spec.rb
@@ -5,7 +5,6 @@ describe FarMar::Vendor do
 
     let (:my_vendor) {FarMar::Vendor.new({})}
     it "must respond to vendor id attribute" do
-
       my_vendor.must_respond_to(:id)
     end
 
@@ -17,19 +16,15 @@ describe FarMar::Vendor do
   describe "self.all" do
     it "must return an array" do
       FarMar::Vendor.all.must_be_instance_of(Array)
-
     end
 
     it "has a Vendor as its first element" do
-
       FarMar::Vendor.all[0].must_be_instance_of(FarMar::Vendor)
     end
   end
 
   describe "self.find" do
     it "returns a Vendor instance" do
-
-
       FarMar::Vendor.find(123).must_be_instance_of(FarMar::Vendor)
     end
   end
