6b2fa83bec7441c26c3a30c236de4bb83bce4e97
Kelly Tran <trankphuong@gmail.com>
2016-09-09 15:50:18 -0700

    Completed FarMar::Vendor methods and their specs

--- lib/farmar_vendor.rb
+++ lib/farmar_vendor.rb
@@ -1,8 +1,3 @@
-require 'csv'
-require_relative 'farmar_market'
-require_relative 'farmar_product'
-require_relative 'farmar_sale'
-
 module FarMar
     class Vendor
         attr_reader :id, :name, :num_of_employees, :market_id
@@ -37,33 +32,26 @@ module FarMar
 
         #market: returns the FarMar::Market instance that is associated with this vendor using the FarMar::Vendor market_id field
         def market
-            market_list = FarMar::Market.all
-            market_by_market_id = market_list.group_by {|i| i.id } # this is the instance variable @id in Farmar::Market class
-            current_market_id = @market_id
-            market_by_market_id[current_market_id]
+            return FarMar::Market.find(@market_id)
         end
 
         #products: returns a collection of FarMar::Product instances that are associated by the FarMar::Product vendor_id field.
         def products
-            product_list = FarMar::Product.all
-            product_by_vendor_id = product_list.group_by {|i| i.vendor_id } # this is the instance variable @vendor_id in FarMar::Product class
-            current_vendor_id = @id
-            product_by_vendor_id[current_vendor_id]
+            products_list = FarMar::Product.all
+            products_by_vendor_id = products_list.select {|i| i.vendor_id == @id }
+            return products_by_vendor_id
         end
 
         #sales: returns a collection of FarMar::Sale instances that are associated by the vendor_id field.
         def sales
-            sale_list = FarMar::Sale.all
-            sale_by_vendor_id = sale_list.group_by {|i| i.vendor_id } # this is the instance variable @vendor_id in FarMar::Sale class
-            current_vendor_id = @id
-            sale_by_vendor_id[current_vendor_id]
+            sales_list = FarMar::Sale.all
+            sales_by_vendor_id = sales_list.select {|i| i.vendor_id == @id }
+            return sales_by_vendor_id
         end
 
         #revenue: returns the the sum of all of the vendor's sales (in cents)
         def revenue
-            # retrieve the list of Sales instances that are associated with this vendor [array]
             sales_of_current_vendor_id = sales
-            # iterate thru each instance in the array, and retrieve the sale amount of each instance
             revenue = 0
             sales_of_current_vendor_id.each do |item|
                 revenue += item.amount
@@ -74,13 +62,9 @@ module FarMar
         #self.by_market(market_id): returns all of the vendors with the given market_id
         def self.by_market(market_id)
             vendors_in_current_market = []
-            self.all.each do |item|
-                if item.market_id == market_id
-                    vendors_in_current_market << item.name
-                end
-            end
+            self.all.each { |item| vendors_in_current_market << item.name if item.market_id == market_id }
             len = vendors_in_current_market.length
-            puts "There are #{len} vendors with the given market_id #{market_id}"
+            puts "There are #{len} vendors with the given Market ID #{market_id}"
             return vendors_in_current_market
         end
     end
--- specs/farmar_vendor_spec.rb
+++ specs/farmar_vendor_spec.rb
@@ -1,14 +1,14 @@
 require_relative 'spec_helper'
 
 describe FarMar::Vendor do
+    let(:my_vendor) { FarMar::Vendor.new(456, 'VendorKelly', 5, 123) }
+
     describe "#initialize" do
         it "should return the correct values of instance variables that were established after initializing" do
-            # id, name, num_of_employees, market_id
-            a = FarMar::Vendor.new(456, 'VendorKelly', 5, 123)
-            a.id.must_equal(456)
-            a.name.must_equal('VendorKelly')
-            a.num_of_employees.must_equal(5)
-            a.market_id.must_equal(123)
+            my_vendor.id.must_equal(456)
+            my_vendor.name.must_equal('VendorKelly')
+            my_vendor.num_of_employees.must_equal(5)
+            my_vendor.market_id.must_equal(123)
         end
     end
 
@@ -24,6 +24,45 @@ describe FarMar::Vendor do
         it "should return an instance of the object where the value of the id field in the CSV matches the passed parameter" do
             FarMar::Vendor.find(1).name.must_equal("Feil-Farrell")
         end
+    end
 
+    describe "#market" do
+        it "should return the FarMar::Market instance that is associated with this vendor using the FarMar::Vendor market_id field" do
+            my_vendor.market.name.must_equal("High Desert Growers' Farmers' Market")
+        end
+    end
+
+    describe "#products" do
+        let(:my_vendors_products) { my_vendor.products }
+
+        it "returns a collection of FarMar::Product instances that are associated by the FarMar::Product vendor_id field" do
+            my_vendors_products.length.must_equal(4)
+            my_vendors_products.first.name.must_equal("Cooing Honey")
+            my_vendors_products.last.name.must_equal("Salty Mushrooms")
+        end
+    end
+
+    describe "#sales" do
+        let(:my_vendors_sales) { my_vendor.sales }
+
+        it "returns a collection of FarMar::Sale instances that are associated by the vendor_id field" do
+            my_vendors_sales.length.must_equal(8)
+            my_vendors_sales.first.amount.must_equal(3790)
+            my_vendors_sales.last.amount.must_equal(7472)
+        end
+    end
+
+    describe "#revenue" do
+        it "returns the the sum of all of the vendor's sales (in cents)" do
+            my_vendor.revenue.must_equal(55225)
+        end
+    end
+
+    describe "self.by_market(market_id)" do
+        it "returns all of the vendors with the given market_id" do
+            b = FarMar::Vendor.by_market(123)
+            b.length.must_equal(6)
+            b.must_equal(["Nolan Group", "Runolfsson, Greenholt and Rutherford", "Daugherty, Bogisich and Crist", "Spinka-Rowe", "Keebler-Kessler", "Greenholt, Hickle and Kozey"])
+        end
     end
 end
