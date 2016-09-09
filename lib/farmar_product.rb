281a29f5f201e516334ea64585aba249773131c1
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-09-09 15:00:37 -0700

    Refactor most product method to simplify.

--- lib/farmar_product.rb
+++ lib/farmar_product.rb
@@ -19,13 +19,13 @@ class Product
       products[id] = self.new(id, name, vendor_id)
 
       # products[line[0].to_i] = (self.new(id: line[0].to_i, name: line[1], vendor_id: line[2].to_i))
+      # ^^ I want this to work but I can't seem to find what's going wrong with the process.
     end
     return products
   end
 
   def self.find(id)
-    p = Product.all
-    return p[id]
+    return all[id]
   end
 
   def vendor
@@ -33,25 +33,20 @@ class Product
   end
 
   def sales
-    s = Sale.all
-    sale_instances = s.find_all { |n| n[1].product_id == @id }
-    return sale_instances
+    return Sale.all.values.find_all { |n| n.product_id == @id }
   end
 
   def number_of_sales
-    s = Sale.all
-    sale_instances = s.find_all { |n| n[1].product_id == @id }
+    sale_instances = Sale.all.values.find_all { |n| n.product_id == @id }
+
     return sale_instances.length
   end
 
   def self.by_vendor(vendor_id)
-    p = all.find_all { |n| n[1].vendor_id == vendor_id }
-
     product_list = []
-    p.length.times do |i|
-      product_list << p[i][1]
-    end
-    return product_list
+    product_list << all.values.find_all { |n| n.vendor_id == vendor_id }
+
+    return product_list.flatten
   end
 
 end
