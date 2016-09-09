4f5d0f7a788e2882f498b77b0cfd18e8d7004788
Yeni <yenicapote08@gmail.com>
2016-09-09 11:41:28 -0700

    cleaned up spec for product and added let(:var)

--- specs/product_spec.rb
+++ specs/product_spec.rb
@@ -1,9 +1,10 @@
 require_relative 'spec_helper'
 
 describe FarMar::Product do
+  let(:green_apple) {FarMar::Product.find(5)}
 
   describe "#initialize" do
-    fruit = FarMar::Product.new(123,"Veg Cheez", 5)
+    let(:fruit) {FarMar::Product.new(123,"Veg Cheez", 5)}
 
     it "should create an instance of Product" do
         fruit.must_be_instance_of(FarMar::Product)
@@ -11,7 +12,7 @@ describe FarMar::Product do
   end
 
   describe "self.all" do
-    fruit = FarMar::Product.all
+    let(:fruit) {FarMar::Product.all}
 
     it "should return a collection of instances in a hash" do
         fruit.must_be_instance_of(Hash)
@@ -19,37 +20,39 @@ describe FarMar::Product do
   end
 
   describe "self.find(id)" do
+    let(:fruit) {FarMar::Product.find(2)}
+
     it "should return an instance of the object matching the id passed" do
-      fruit = FarMar::Product.find(2)
       fruit.id.must_equal(2)
     end
   end
 
   describe "#vendor" do
+
     it "should return the vendor instance associated with this product" do
-      green_apple = FarMar::Product.find(5)
       fruit_vendor = FarMar::Vendor.find(4)
       green_apple.vendor.must_equal(fruit_vendor)
     end
   end
 
   describe "#sales" do
+
     it "should return a collection of sales associated with this product" do
-      green_apple = FarMar::Product.find(5)
       green_apple.sales.length.must_equal(2)
     end
   end
 
   describe "#number_of_sales" do
+
     it "should return the number of times the product has been sold" do
-      green_apple = FarMar::Product.find(5)
       green_apple.number_of_sales.must_equal(2)
     end
   end
 
   describe "self.by_vendor(vendor_id)" do
+    let(:veg_vendor) {FarMar::Product.by_vendor(4)}
+
     it "should return all of the products associated with that vendor id" do
-      veg_vendor = FarMar::Product.by_vendor(4)
       veg_vendor.length.must_equal(3)
     end
   end
