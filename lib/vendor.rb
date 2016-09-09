467ea355916a53f59ce4c39f8e121d55ce22355d
Yasmin <yasminor@gmail.com>
2016-09-09 09:38:43 -0700

    finished methods for vendor

--- lib/vendor.rb
+++ lib/vendor.rb
@@ -13,7 +13,7 @@ module FarMar
     def self.all
       vendor_array = []
       CSV.read("/Users/yasminor/ada/Week_5/FarMar/support/vendors.csv").each_with_index do |line, i|
-        vendor_array[i] = self.new(line[0], line[1], line[2], line[3])
+        vendor_array[i] = self.new(line[0].to_i, line[1], line[2].to_i, line[3].to_i)
       end
       return vendor_array
     end
@@ -21,7 +21,7 @@ module FarMar
     def self.find(id)
       vendor_obj_array = FarMar::Vendor.all
       vendor_obj_array.map do |vendor|
-        if vendor.id.to_i == id
+        if vendor.id == id
           return vendor
         end
       end
@@ -31,7 +31,56 @@ module FarMar
       FarMar::Market.find(@market_id)
     end
 
+    def products #returns a collection of FarMar::Product instances that are associated by the FarMar::Product vendor_id field
+      FarMar::Product.by_vendor(@id)
+      # vendor_products_array = []
+      # products_array = FarMar::Product.all
+      # products_array.map do |product|
+      #   if product.vendor_id == @id
+      #     vendor_products_array << product
+      #   end
+      # end
+      # return vendor_products_array
+    end
 
+    def sales
+      vendor_sales_array = []
+      sales_list = FarMar::Sale.all
+      sales_list.map do |sale|
+        if sale.vendor_id == @id
+          vendor_sales_array << sale
+        end
+      end
+      return vendor_sales_array
+    end
+
+    def revenue #returns the sum of all of a particular vendor's sales
+      vendor_sales_array = []
+      sales_list = FarMar::Sale.all
+      sales_list.map do |sale|
+        if sale.vendor_id == @id
+          vendor_sales_array << sale
+        end
+      end
+
+      amounts_array = []
+      vendor_sales_array.map do |vendor_sale|
+        amounts_array << vendor_sale.amount
+      end
+
+      sum = amounts_array.reduce(:+).to_i
+    end
+
+  def self.by_market(market_id) # returns all the vendors with a given market_id
+    market_vendors = []
+    all_vendors = FarMar::Vendor.all
+    all_vendors.map do |vendor|
+      if vendor.market_id == market_id
+        market_vendors << vendor
+      end
+    end
+    return market_vendors
+  end
 
   end
 end
