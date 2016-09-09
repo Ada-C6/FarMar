e03cb01edd4b98b1df51820f95ad98084918e1b1
Jennifer Owens <jmo.jennifer@gmail.com>
2016-09-09 14:17:46 -0700

    Refactored Sale Class' spec tests to not fail if dataset changes

--- specs/farmar_sale_spec.rb
+++ specs/farmar_sale_spec.rb
@@ -1,63 +1,73 @@
 require_relative 'spec_helper'
 
 describe FarMar::Sale do
-  # would need to change if dataset were to change
-  let(:sale) { FarMar::Sale.new({sale_id: 1, sale_amount: 9290, purchase_time: '2013-11-07 04:34:56', vendor_id: 1, product_id: 1, market_zip: 0}) }
+  before (:each) do
+    @all_sales = FarMar::Sale.all
+    sale_hash = {}
+    sale_csv_file = CSV.read("../FarMar/support/sales.csv")
+    sale_csv_file.each do |sale|
+      sale_hash[:sale_id] = sale[0].to_i
+      sale_hash[:sale_amount] = sale[1].to_i
+      sale_hash[:sale_purchase_time] = DateTime.parse(sale[2])
+      sale_hash[:vendor_id] = sale[3].to_i
+      sale_hash[:product_id] = sale[4].to_i
+    end
+    @sale = FarMar::Sale.new(sale_hash)
+  end
 
   describe "#initialize" do
-    # considered with variable dataset in mind
     it "can create a new instance of Sale" do
-      sale.must_be_instance_of(FarMar::Sale)
+      @sale.must_be_instance_of(FarMar::Sale)
     end
   end
 
   describe "all" do
-    # would need to change if dataset were to change
+    # if time after total refactoring, would like to see if there's additional things to check to make this more useful
     it "should put all values of its CSV file into an array" do
-      FarMar::Sale.all.length.must_equal(12798)
+      @all_sales.wont_be_empty
     end
   end
 
   describe "find(id)" do
-    # would need to change if dataset were to change
     it "should let the user know if the id is not present" do
-      proc { FarMar::Sale.find(12002) }.must_raise("ID was not present")
+      test_hash = {}
+      test_hash[:sale_id] = @all_sales.length + 1
+      test_hash[:sale_amount] = "Test"
+      test_hash[:sale_purchase_time] = '2013-11-07 23:58:00 -0800'
+      test_hash[:vendor_id] = 0
+      test_hash[:product_id] = 0
+
+      proc { FarMar::Sale.find(sale_id) }.must_raise("ID was not present")
     end
 
-    # would need to change if dataset were to change
     it "should find a specific Sale by the sale_id" do
-      FarMar::Sale.all
-      found_sale = FarMar::Sale.find(1)
-      found_sale.sale_id.must_equal(1)
-      found_sale.sale_amount.must_equal(sale.sale_amount)
+      found_sale = FarMar::Sale.find(@sale.sale_id)
+      found_sale.sale_id.must_equal(@sale.sale_id)
+      found_sale.sale_amount.must_equal(@sale.sale_amount)
     end
   end
 
   describe "#vendor" do
-    # would need to change if dataset were to change
     it "return the FarMar::Vendor instance that is associated with this sale using the FarMar::Sale vendor_id field" do
-      FarMar::Vendor.all
-      found_vendor = FarMar::Vendor.find(1)
-      sale.vendor.must_equal(found_vendor.vendor_name)
+      found_vendor = FarMar::Vendor.find(@sale.vendor_id)
+      @sale.vendor.must_equal(found_vendor.vendor_name)
     end
   end
 
   describe "#product" do
-    # would need to change if dataset were to change
     it "return the FarMar::Product instance that is associated with this sale using the FarMar::Sale product_id field" do
       FarMar::Product.all
-      found_product = FarMar::Product.find(1)
-      sale.product.must_equal(found_product.product_name)
+      found_product = FarMar::Product.find(@sale.product_id)
+      @sale.product.must_equal(found_product.product_name)
     end
   end
 
   describe "#self.between(beginning_time, end_time)" do
-    # would need to change if dataset were to change
+    # if time after total refactoring, would like to see if there's additional things to check to make this more useful
     it "should returns a collection of FarMar::Sale objects where the purchase time is between the two times given as arguments" do
-      FarMar::Sale.all
       test_start = '2013-11-07 23:58:00 -0800'
       test_end = '2013-11-07 23:59:59 -0800'
-      FarMar::Sale.between(test_start, test_end).length.must_equal(22)
+      FarMar::Sale.between(test_start, test_end).wont_be_empty
     end
   end
 end
