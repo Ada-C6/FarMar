32fb7fc02e665ed0edabca74043c21b61875be5c
jm-rives <joanna.rives@gmail.com>
2016-09-20 13:03:58 -0700

    ADD tests that market method returns associated market

--- specs/Vendor_spec.rb
+++ specs/Vendor_spec.rb
@@ -49,6 +49,9 @@ describe "Testing vendor" do
   # testing the additiona FarMar::Vendor methods
   it "Tests that the instance method named market returns the  FarMar::Market instance associated with the vendor using FarMar::Vendor.market_id field" do
     new_vendor = FarMar::Vendor.find('474')
+    new_vendor.market_id.must_equal '89'
+
+
   end
 
 end
